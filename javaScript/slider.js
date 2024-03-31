605c3e54f92a300d4ab695d6603b4cbda1081e62
elmrys <elmrys@gmail.com>
2024-03-31 03:30:26 +0200

    changed spawn position

---
+++ javaScript/slider.js
@@ -0,0 +1,9 @@
+const mediaURL = '../media/'
+const images = [mediaURL + 'slider1.png', mediaURL + 'slider2.png', mediaURL + 'slider3.png', mediaURL + 'slider4.png']
+
+const videoURL = 'https://githubpagesvideos.s3.eu-north-1.amazonaws.com/sliderVideo.mp4'
+const isVideo = false
+
+const center = document.querySelector('#center')
+const left = document.querySelector('#left')
+const right = document.querySelector('#right')
