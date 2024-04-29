354171391e5c0f86a1311837fae0398f2d6a68c0
Rios Maël <mael.rios@telecom-paris.fr>
2024-04-29 16:14:13 +0200

    added kiss marry kill

--- javaScript/animation.js
+++ javaScript/animation.js
@@ -8,10 +8,10 @@ let GoLeft = false;
 
 const mediaUrl = '../media/';
 const htmlUrl = '../HTMLContents/';
-const projectImages = [mediaUrl + 'projects1.png', mediaUrl + 'projects2.png', mediaUrl + 'OpenGLHeader.png', mediaUrl + 'steamRoom.png', mediaUrl + 'ecolife.png', mediaUrl + 'slider1.png']
-const projectDescriptions = ['3D Robot Simulator', 'Alice the WonderGame', 'OpenGL Projects', 'Rooms.XYZ', 'Ecolife', 'Slider\'s Adventure']
-const projectDescriptionSizes = ['500%', '450%', '500%', '500%', '500%', '500%']
-const URLS = [htmlUrl + '3DSimulator.html', 'https://matelou.itch.io/alice-the-wonder-game', htmlUrl + 'OpenGL.html', 'https://rooms.xyz/elmrysmordred/steamroom', htmlUrl + 'ecolife.html', htmlUrl + 'slider.html'];
+const projectImages = [mediaUrl + 'projects1.png', mediaUrl + 'projects2.png', mediaUrl + 'OpenGLHeader.png', mediaUrl + 'steamRoom.png', mediaUrl + 'ecolife.png', mediaUrl + 'slider1.png', mediaUrl + 'kmk.png']
+const projectDescriptions = ['3D Robot Simulator', 'Alice the WonderGame', 'OpenGL Projects', 'Rooms.XYZ', 'Ecolife', 'Slider\'s Adventure', 'Kiss Marry Kill']
+const projectDescriptionSizes = ['500%', '450%', '500%', '500%', '500%', '500%', '500%']
+const URLS = [htmlUrl + '3DSimulator.html', 'https://matelou.itch.io/alice-the-wonder-game', htmlUrl + 'OpenGL.html', 'https://rooms.xyz/elmrysmordred/steamroom', htmlUrl + 'ecolife.html', htmlUrl + 'slider.html', 'https://matelou.itch.io/kiss-marry-kill'];
 // Interactive Div
 let interactiveDiv = null;
 let ProjectTitle = null;
@@ -132,6 +132,7 @@ function initiateButtons(){
     const WhichSelectionButton4 = document.getElementById('WhichSelectionButton4');
     const WhichSelectionButton5 = document.getElementById('WhichSelectionButton5');
     const WhichSelectionButton6 = document.getElementById('WhichSelectionButton6');
+    const WhichSelectionButton7 = document.getElementById('WhichSelectionButton7');
     
     SelectionButtons.push(WhichSelectionButton1);
     SelectionButtons.push(WhichSelectionButton2);
@@ -139,6 +140,7 @@ function initiateButtons(){
     SelectionButtons.push(WhichSelectionButton4);
     SelectionButtons.push(WhichSelectionButton5);
     SelectionButtons.push(WhichSelectionButton6);
+    SelectionButtons.push(WhichSelectionButton7);
     SelectionButtons.forEach((button) => {
         button.addEventListener('click', () => {
             WhichSelectionButtonClicked(button)
