2637bfa88703a5c4b08fb19a0d435e8a3df1e8bc
Elmrys <elmrys@gmail.com>
2026-02-08 19:13:43 +0100

    Updates

--- projects/skyegrid/Skyegrid.js
+++ projects/skyegrid/Skyegrid.js
@@ -84,7 +84,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\elmry\AppData\Local\Temp\tmpbfbwass5.js
+// include: C:\Users\elmry\AppData\Local\Temp\tmp7s5vmfnj.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -218,21 +218,21 @@ Module['FS_createPath']("/", "data", true, true);
 
   })();
 
-// end include: C:\Users\elmry\AppData\Local\Temp\tmpbfbwass5.js
-// include: C:\Users\elmry\AppData\Local\Temp\tmp_ekndcjq.js
+// end include: C:\Users\elmry\AppData\Local\Temp\tmp7s5vmfnj.js
+// include: C:\Users\elmry\AppData\Local\Temp\tmpj0f1gj5j.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\elmry\AppData\Local\Temp\tmp_ekndcjq.js
-// include: C:\Users\elmry\AppData\Local\Temp\tmp06zfiqp2.js
+  // end include: C:\Users\elmry\AppData\Local\Temp\tmpj0f1gj5j.js
+// include: C:\Users\elmry\AppData\Local\Temp\tmpd8yn3srf.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\elmry\AppData\Local\Temp\tmp06zfiqp2.js
+  // end include: C:\Users\elmry\AppData\Local\Temp\tmpd8yn3srf.js
 
 
 var arguments_ = [];
