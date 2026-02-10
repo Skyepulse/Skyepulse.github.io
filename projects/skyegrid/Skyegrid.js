64929e9283b822b1678af817ea7b4b0c0752d8a4
Elmrys <elmrys@gmail.com>
2026-02-10 15:42:00 +0100

    Updates

--- projects/skyegrid/Skyegrid.js
+++ projects/skyegrid/Skyegrid.js
@@ -84,7 +84,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\elmry\AppData\Local\Temp\tmpli8syrit.js
+// include: C:\Users\elmry\AppData\Local\Temp\tmpn1jnm8xp.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -218,21 +218,21 @@ Module['FS_createPath']("/", "data", true, true);
 
   })();
 
-// end include: C:\Users\elmry\AppData\Local\Temp\tmpli8syrit.js
-// include: C:\Users\elmry\AppData\Local\Temp\tmpju3j1r11.js
+// end include: C:\Users\elmry\AppData\Local\Temp\tmpn1jnm8xp.js
+// include: C:\Users\elmry\AppData\Local\Temp\tmpi9syk9d_.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\elmry\AppData\Local\Temp\tmpju3j1r11.js
-// include: C:\Users\elmry\AppData\Local\Temp\tmpgzq__tl2.js
+  // end include: C:\Users\elmry\AppData\Local\Temp\tmpi9syk9d_.js
+// include: C:\Users\elmry\AppData\Local\Temp\tmp51j1bgvl.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\elmry\AppData\Local\Temp\tmpgzq__tl2.js
+  // end include: C:\Users\elmry\AppData\Local\Temp\tmp51j1bgvl.js
 
 
 var arguments_ = [];
@@ -10025,7 +10025,7 @@ function checkIncomingModuleAPI() {
   ignoredModuleProp('fetchSettings');
 }
 var ASM_CONSTS = {
-  166584: () => { self.specialHTMLTargets && (specialHTMLTargets["!canvas"] = Module.canvas) }
+  166600: () => { self.specialHTMLTargets && (specialHTMLTargets["!canvas"] = Module.canvas) }
 };
 function ImGui_ImplGlfw_EmscriptenOpenURL(url) { url = url ? UTF8ToString(url) : null; if (url) window.open(url, '_blank'); }
 
